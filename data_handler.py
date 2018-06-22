c3eff63169724906c47a9a1269e42bc9ece85912
naiveHobo <sarthakmittal2608@gmail.com>
2018-06-22 11:15:58 +0530

    DataHandler is compatible with torch Dataset

--- data_handler.py
+++ data_handler.py
@@ -1,3 +1,5 @@
+import torch
+import torch.utils.data as data
 import numpy as np
 from tqdm import tqdm
 from nltk.tokenize import word_tokenize
@@ -5,11 +7,12 @@ from PIL import Image
 import os
 import cPickle as pkl
 import gzip
+import re
 
 
-class DataHandler:
+class DataHandler(data.Dataset):
 
-    def __init__(self, data=None, max_len=20, augment=False):
+    def __init__(self, data=None, max_len=20):
         self.data = data
         self.max_length = max_len
         self.vocab = {}
@@ -20,27 +23,27 @@ class DataHandler:
         self.UNKNOWN = '<unk>'
         self.START = '<start>'
         self.END = '<end>'
-        self.augment = augment
-        self.mean = None
+        self.augment = None
+        self.mean = None #np.array([0.45803204, 0.4461004, 0.4039198])
+        self.std = None #np.array([0.24218813, 0.23319727, 0.23719482])
 
-    # def __getitem__(self, index):
-    #     """Returns an image and caption pair from the dataset"""
-    #
-    #     path = self.data['filename'].iloc[index]
-    #     caption = self.data['caption'].iloc[index]
-    #
-    #     image = Image.open(path).convert('RGB')
-    #     if self.augment is not None:
-    #         image = self.transform(image)
-    #
-    #     # Convert caption (string) to word ids.
-    #     tokens = nltk.tokenize.word_tokenize(str(caption).lower())
-    #     caption = []
-    #     caption.append(vocab('<start>'))
-    #     caption.extend([vocab(token) for token in tokens])
-    #     caption.append(vocab('<end>'))
-    #     target = torch.Tensor(caption)
-    #     return image, target
+    def __getitem__(self, index):
+        """Returns an image and caption pair from the dataset"""
+
+        path = self.data['filename'].iloc[index]
+        caption = self.data['caption'].iloc[index]
+
+        image = Image.open(path).convert('RGB')
+        if self.augment is not None:
+            image = self.augment(image)
+
+        caption = caption.decode('utf').lower()
+        caption = re.sub(r'[^\w\s\<\>]', '', caption)
+        tokens = caption.split(' ')
+        caption = [self.get_word_id(token) for token in tokens]
+        target = torch.Tensor(caption)
+
+        return image, target
 
     def read(self, data, max_len=20):
         """Read DataFrame"""
@@ -61,6 +64,7 @@ class DataHandler:
         with tqdm(total=self.data.shape[0]) as pbar:
             for i, row in self.data.iterrows():
                 caption = row['caption'].decode('utf').lower()
+                caption = re.sub(r'[^\w\s\']', '', caption)
 
                 tokens = word_tokenize(caption)
                 tokens = tokens[:self.max_length]
@@ -86,14 +90,15 @@ class DataHandler:
 
         with tqdm(total=self.data.shape[0]) as pbar:
             for i, row in self.data.iterrows():
-                caption = row['review']
+                caption = row['caption'].decode('utf').lower()
+                caption = re.sub(r'[^\w\s\']', '', caption)
                 tokens = word_tokenize(caption)
+                tokens = [self.START] + tokens + [self.END]
 
-                for _ in range(self.max_length - len(tokens)):
+                for _ in range(self.max_length - len(tokens) + 2):
                     tokens.append(self.PAD)
 
                 caption = " ".join(tokens)
-                caption = " ".join([self.START, caption, self.END])
 
                 self.data.at[i, 'caption'] = caption
 
@@ -102,14 +107,17 @@ class DataHandler:
         print "\nPadding was successful!"
 
     def resize_images(self, out_path, size=256):
-        """Resizes images in the dataset"""
+        """Resizes images in the dataset and calculates mean and standard deviation"""
 
         assert self.data is not None, "No data has been loaded yet, call DataHandler.read()"
 
         if not os.path.exists(out_path):
             os.makedirs(out_path)
 
-        print "\nResizing images..."
+        self.mean = np.zeros(3)
+        self.std = np.zeros(3)
+
+        print "\nResizing images and calculating mean and standard deviation..."
 
         with tqdm(total=self.data.shape[0]) as pbar:
             for i, row in self.data.iterrows():
@@ -117,30 +125,18 @@ class DataHandler:
                 img_name = path.split('/')[-1]
                 image = Image.open(path).convert('RGB')
                 image = image.resize([size, size], Image.ANTIALIAS)
-                image.save(os.path.join(out_path, img_name), image.format)
+                # image.save(os.path.join(out_path, img_name), image.format)
                 self.data.at[i, 'filename'] = os.path.join(out_path, img_name)
 
-                image = np.array(image, dtype=np.float32)
+                image = np.array(image)
 
-                print np.mean(image, axis=2).shape
+                self.mean += np.mean(image, axis=(0, 1))
+                self.std += np.std(image, axis=(0, 1))
 
                 pbar.update(1)
 
-    # def get_mean(self):
-    #     """Returns the mean and standard deviation across all channels for the dataset"""
-    #
-    #     assert self.data is not None, "No data has been loaded yet, call DataHandler.read()"
-    #
-    #     if self.mean is not None:
-    #         return self.mean
-    #
-    #     print "\nCalculating mean and standard deviation..."
-    #
-    #     with tqdm(total=self.data.shape[0]) as pbar:
-    #         for i, row in self.data.iterrows():
-    #             path = './data/Flickr8k_Dataset/Flicker8k_Dataset/' + row['filename']
-    #             image = np.array(Image.open(path).convert('RGB'), dtype=np.float32)
-    #             print image.shape
+        self.mean /= self.data.shape[0] * 255
+        self.std /= self.data.shape[0] * 255
 
     def load_embeddings(self, embed_path, embed_size=300):
         """Loads pre-trained embeddings"""
@@ -196,3 +192,7 @@ class DataHandler:
         self.word_id = data['wordmap']
 
         print "\nSuccessfully loaded data from {}".format(path)
+
+    def set_augment(self, transform):
+        """Sets transform for dataset"""
+        self.augment = transform
